a64b673b321fcd2894b4e0bbf65b2aff2be1d098
Yin Jifeng <jifeng.yin@gmail.com>
2019-03-18 18:25:48 +0800

    fix: postConstruct takes no parameters

--- spring-jcache-refresh/src/main/java/org/springframework/cache/jcache/config/JCacheRefreshConfiguration.java
+++ spring-jcache-refresh/src/main/java/org/springframework/cache/jcache/config/JCacheRefreshConfiguration.java
@@ -1,21 +1,22 @@
 package org.springframework.cache.jcache.config;
 
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.aspectj.JCacheCacheAspect;
 import org.springframework.cache.jcache.interceptor.CacheRefreshResultInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.Assert;
 import org.springframework.util.ReflectionUtils;
 
-import javax.annotation.PostConstruct;
 import java.lang.reflect.Field;
 
 @Configuration
 @EnableConfigurationProperties(JCacheRefreshProperties.class)
 public class JCacheRefreshConfiguration {
 
-    @PostConstruct
-    public void updateInterceptor(JCacheRefreshProperties properties) {
+    @Bean
+    public JCacheManagerCustomizer customizer(JCacheRefreshProperties properties) {
         JCacheCacheAspect aspect = JCacheCacheAspect.aspectOf();
         Field field = ReflectionUtils.findField(JCacheCacheAspect.class, "cacheResultInterceptor");
         Assert.notNull(field, "cacheResultInterceptor field");
@@ -24,5 +25,8 @@ public class JCacheRefreshConfiguration {
                 properties.getExternalExpiry(), properties.executionTimeout, aspect.getErrorHandler());
         ReflectionUtils.setField(field, aspect, interceptor);
         field.setAccessible(false);
+
+        return cacheManager -> {
+        };
     }
 }
