bb3dccd35c353d7865b86e3fbaee8487ace450c7
Yin Jifeng <jifeng.yin@gmail.com>
2019-03-18 17:47:59 +0800

    add inFlight check

--- spring-jcache-refresh/src/main/java/org/springframework/cache/jcache/interceptor/CacheRefreshResultInterceptor.java
+++ spring-jcache-refresh/src/main/java/org/springframework/cache/jcache/interceptor/CacheRefreshResultInterceptor.java
@@ -5,13 +5,17 @@ import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.cache.interceptor.CacheOperationInvocationContext;
 import org.springframework.cache.interceptor.CacheOperationInvoker;
 import org.springframework.cache.interceptor.CacheResolver;
-import org.springframework.cache.jcache.JCacheRefreshCache;
 import org.springframework.cache.jcache.JCacheCache;
+import org.springframework.cache.jcache.JCacheRefreshCache;
 import org.springframework.cache.jcache.support.EpochValueWrapper;
 import org.springframework.lang.Nullable;
 import reactor.core.publisher.Mono;
 
+import javax.cache.CacheManager;
+import javax.cache.Caching;
 import javax.cache.configuration.CompleteConfiguration;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.expiry.CreatedExpiryPolicy;
 import javax.cache.expiry.ExpiryPolicy;
 import java.time.Duration;
 import java.util.concurrent.ConcurrentHashMap;
@@ -24,12 +28,14 @@ public class CacheRefreshResultInterceptor extends CacheResultInterceptor {
     private final Duration executionTimeout;
 
     private final ConcurrentMap<String, Cache> cacheMap = new ConcurrentHashMap<>(16);
+    private final javax.cache.Cache<String, Boolean> bustCache;
 
     public CacheRefreshResultInterceptor(double expiryFactor, Duration externalExpiry, Duration executionTimeout, CacheErrorHandler errorHandler) {
         super(errorHandler);
         this.expiryFactor = expiryFactor;
         this.externalExpiry = externalExpiry;
         this.executionTimeout = executionTimeout;
+        this.bustCache = createBustCache(executionTimeout);
     }
 
     @Override
@@ -47,12 +53,12 @@ public class CacheRefreshResultInterceptor extends CacheResultInterceptor {
             if (cachedValue != null) {
                 // extra check
                 if (cachedValue instanceof EpochValueWrapper) {
-                    if (((EpochValueWrapper) cachedValue).isExpired()) {
+                    if (((EpochValueWrapper) cachedValue).isExpired() && !inFlight(cache, cacheKey)) {
                         Mono<Object> mono = Mono.fromRunnable(() -> invoke(context, invoker, cache, cacheKey)).ignoreElement();
                         if (!executionTimeout.isZero()) {
                             mono = mono.timeout(executionTimeout);
                         }
-                        mono.subscribe();
+                        mono.doOnTerminate(() -> bustCache.remove(flightKey(cache, cacheKey))).subscribe();
                     }
                 }
                 return cachedValue.get();
@@ -118,6 +124,32 @@ public class CacheRefreshResultInterceptor extends CacheResultInterceptor {
             return cache;
         }
         return new JCacheRefreshCache(cache.getNativeCache(), duration, cache.isAllowNullValues());
+    }
+
+
+    private javax.cache.Cache<String, Boolean> createBustCache(Duration executionTimeout) {
+        CacheManager manager = Caching.getCachingProvider().getCacheManager();
+        MutableConfiguration<String, Boolean> config = new MutableConfiguration<>();
+        if (executionTimeout.isZero()) {
+            executionTimeout = Duration.ofSeconds(15);
+        }
+        javax.cache.expiry.Duration duration = new javax.cache.expiry.Duration(TimeUnit.MILLISECONDS, executionTimeout.toMillis());
+        config.setExpiryPolicyFactory(CreatedExpiryPolicy.factoryOf(duration));
+        return manager.createCache("spring-jcache-refresh", config);
+    }
+
+    private String flightKey(Cache cache, Object cacheKey) {
+        return cache.getName() + "--" + cacheKey.hashCode();
+    }
+
+    private boolean inFlight(Cache cache, Object cacheKey) {
+        String key = flightKey(cache, cacheKey);
+        Boolean inflight = bustCache.get(key);
+        if (Boolean.TRUE.equals(inflight)) {
+            return true;
+        }
+        bustCache.put(key, true);
+        return false;
     }
 
     @Nullable
